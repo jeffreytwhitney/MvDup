b531c6154722df26ff201d9a38147fd99bc369cf
JTWhitney <jeffrey.t.whitney@gmail.com>
2024-11-06 14:39:57 -0600

    Fixes

--- MvDuplicate.py
+++ MvDuplicate.py
@@ -3,20 +3,24 @@ import os
 import shutil
 import sys
 import time
+from datetime import datetime
 from typing import List
 
 
 def write_error(e: Exception):
+    current_datetime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    complete_error_message = f"{current_datetime}: {e}"
     error_path = os.path.join(resolve_path(), "error_log.txt")
     with open(error_path, "a") as f:
-        f.write(f"{e}\n")
+        f.write(f"{complete_error_message}\n")
 
 
 def write_error_message(error_message: str):
-
+    current_datetime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    complete_error_message = f"{current_datetime}: {error_message}"
     error_path = os.path.join(resolve_path(), "error_log.txt")
     with open(error_path, "a") as f:
-        f.write(f"{error_message}\n")
+        f.write(f"{complete_error_message}\n")
 
 
 def get_index_containing_text(file_lines: list[str], text_to_find: str) -> int:
@@ -35,35 +39,84 @@ def resolve_path():
 
 
 def get_ini_file_path(ini_file_name):
-    current_dir = resolve_path()
-    return current_dir + "\\" + ini_file_name + ".ini"
+    try:
+        current_dir = resolve_path()
+        return current_dir + "\\" + ini_file_name + ".ini"
+
+    except:
+        return ""
 
 
 def get_stored_ini_value(ini_section, ini_key, ini_filename):
+    config_value = ""
     ini_file_path = get_ini_file_path(ini_filename)
     config = configparser.ConfigParser()
     config.read(ini_file_path)
     try:
         config_value = config.get(ini_section, ini_key)
     except:
-        try:
-            config_value = config.get(ini_section, "*")
-        except:
-            config_value = ""
+        config_value = ""
     return config_value
 
 
 def get_unencoded_file_lines(file_path: str) -> list[str]:
-    if not file_path:
+    try:
+        with open(file_path, "r") as f:
+            return f.readlines()
+    except Exception as e:
+        write_error(e)
         return []
-    with open(file_path, "r") as f:
-        return f.readlines()
 
 
 def write_lines_to_file(output_filepath: str, file_lines: list[str], newline='\n'):
-    with open(output_filepath, 'w+', newline=f'{newline}') as f:
-        for line in file_lines:
-            f.write(f"{line}")
+    try:
+        with open(output_filepath, 'w+', newline=f'{newline}') as f:
+            for line in file_lines:
+                f.write(f"{line}")
+    except Exception as e:
+        write_error(e)
+
+
+def check_file_paths() -> bool:
+    try:
+        ini_filepath = get_ini_file_path("settings")
+        input_path = get_stored_ini_value("Paths", "input_path", "settings")
+        primary_output_path = get_stored_ini_value("Paths", "primary_output_path", "settings")
+        secondary_output_path = get_stored_ini_value("Paths", "secondary_output_path", "settings")
+
+        if not ini_filepath:
+            write_error_message("No ini file path")
+            return False
+        if not os.path.exists(ini_filepath):
+            write_error_message(f"Ini file path '{ini_filepath}' does not exist.")
+            return False
+
+        if not input_path:
+            write_error_message("No input path")
+            return False
+        if not os.path.exists(input_path):
+            write_error_message(f"Input path '{input_path}' does not exist")
+            return False
+
+        if not primary_output_path:
+            write_error_message("No primary output path")
+            return False
+        if not os.path.exists(primary_output_path):
+            write_error_message(f"Primary output path '{primary_output_path}' does not exist")
+            return False
+
+        if not secondary_output_path:
+            write_error_message("No secondary output path")
+            return False
+        if not os.path.exists(secondary_output_path):
+            write_error_message(f"Secondary output path '{secondary_output_path}' does not exist")
+            return False
+
+        return True
+
+    except Exception as e:
+        write_error(e)
+        return False
 
 
 class export_processor:
@@ -152,35 +205,28 @@ class export_processor:
         write_lines_to_file(self._input_filepath, self._file_lines)
 
     def process_export(self):
-        if self._convert_to_prompts:
-            self._format_export_file()
-        if self._duplicate_file:
-            shutil.copyfile(self._input_filepath, self._primary_output_filepath)
-            self._convert_export_to_spc_style()
-            shutil.move(self._input_filepath, self._secondary_output_filepath)
-        else:
-            shutil.move(self._input_filepath, self._primary_output_filepath)
+        try:
+            if self._convert_to_prompts:
+                self._format_export_file()
+            if self._duplicate_file:
+                shutil.copyfile(self._input_filepath, self._primary_output_filepath)
+                self._convert_export_to_spc_style()
+                shutil.move(self._input_filepath, self._secondary_output_filepath)
+            else:
+                shutil.move(self._input_filepath, self._primary_output_filepath)
+        except Exception as e:
+            write_error(e)
 
 
 def main():
-    input_directory = ""
-    try:
+    if check_file_paths():
         input_directory = get_stored_ini_value("Paths", "input_path", "settings")
-    except Exception as e:
-        write_error(e)
-
-    if input_directory:
         while True:
             for filename in os.listdir(input_directory):
                 print(filename)
                 time.sleep(6)
                 export = export_processor(filename)
-                try:
-                    export.process_export()
-                except Exception as e:
-                    write_error(e)
-    else:
-        write_error_message("Can't find .ini file.")
+                export.process_export()
 
 
 if __name__ == "__main__":
